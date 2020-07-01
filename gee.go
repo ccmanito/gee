7761c1f4148044d085720173b109870aae5547a8
ccmanito <648280021@qq.com>
2020-07-01 15:06:35 +0800

    add route func and request func

--- gee.go
+++ gee.go
@@ -1,2 +1,48 @@
 package gee
 
+import (
+	"fmt"
+	"net/http"
+
+)
+
+type HandlerFunc func(http.ResponseWriter, *http.Request)
+
+// Engine 实现了ServeHTTP的接口
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+// NEw 是gee.Engine 的构造函数
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
+	key := method + "-" + pattern
+	engine.router[key] = handler
+}
+
+// GET defines the method to add GET request
+func (engine *Engine) GET(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+// POST defines the method to add POST request
+func (engine *Engine) POST(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+// Run defines the method to start a http server
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) { // ListenAndServe 方法会调用该方法
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(w, req)
+	} else {
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
+}
