938b6c607572d6f934ed94f78a50fcc0bd1684ea
ccmanito <648280021@qq.com>
2020-07-01 18:21:11 +0800

    add Context

--- gee.go
+++ gee.go
@@ -1,26 +1,23 @@
 package gee
 
 import (
-	"fmt"
 	"net/http"
-
 )
 
-type HandlerFunc func(http.ResponseWriter, *http.Request)
+type HandlerFunc func(*Context)
 
 // Engine 实现了ServeHTTP的接口
 type Engine struct {
-	router map[string]HandlerFunc
+	router *router
 }
 
 // NEw 是gee.Engine 的构造函数
 func New() *Engine {
-	return &Engine{router: make(map[string]HandlerFunc)}
+	return &Engine{router: newRouter()}
 }
 
 func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	key := method + "-" + pattern
-	engine.router[key] = handler
+	engine.router.addRoute(method, pattern, handler)
 }
 
 // GET defines the method to add GET request
@@ -39,10 +36,6 @@ func (engine *Engine) Run(addr string) (err error) {
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) { // ListenAndServe 方法会调用该方法
-	key := req.Method + "-" + req.URL.Path
-	if handler, ok := engine.router[key]; ok {
-		handler(w, req)
-	} else {
-		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
-	}
+	c := newContext(w, req)
+	engine.router.handle(c)
 }
